80ef4bdbdef84af2f264203bd606ee3060759836
Louis Phan <louis.phhh@gmail.com>
2022-07-10 14:53:36 +0700

    ignore buid

--- dist/constant/message-pattern.constant.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ACCOUNT_MESSAGE_PATTERN = void 0;
-// account service
-exports.ACCOUNT_MESSAGE_PATTERN = {
-    REGISTER_ACCOUNT: 'registerAccount',
-};
--- dist/constant/nats-queue-name.constant.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ACCOUNT_SERVICE = void 0;
-exports.ACCOUNT_SERVICE = 'account_service';
